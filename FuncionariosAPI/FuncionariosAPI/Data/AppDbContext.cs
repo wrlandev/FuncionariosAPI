3705617b7ebb474ee7b543b5e64cf1daac3ac3fa
Wrlan.Dev <109324822+wrlandev@users.noreply.github.com>
2023-11-21 16:11:18 -0300

    Data

---
+++ FuncionariosAPI/FuncionariosAPI/Data/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using FuncionariosAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace FuncionariosAPI.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<FuncionarioModel> Funcionarios { get; set; }
+    }
+}
